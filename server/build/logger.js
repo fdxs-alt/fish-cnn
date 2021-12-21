9201032d95c6666dc388b841fdfa3df11461d685
Jakub Sukiennik <kuba1207710@gmail.com>
2021-12-21 22:45:02 +0100

    remove build

--- server/build/logger.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const winston_1 = require("winston");
-const logger = (0, winston_1.createLogger)({ transports: [new winston_1.transports.Console()] });
-exports.logger = logger;
